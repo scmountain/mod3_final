b21e4f54393a48e2d7ef9a4613ef5da380c6b399
Spencer Carter <scmountain@gmail.com>
2017-04-13 09:29:55 -0600

    adds requests show

--- spec/requests/api/v1/user_can_send_requests_spec.rb
+++ spec/requests/api/v1/user_can_send_requests_spec.rb
@@ -16,4 +16,19 @@ describe 'as a user I can send' do
     expect(items.last["name"]).to eq("pig")
     expect(items.last["name"]).to_not eq("chicken")
   end
+
+  it "can send request for 1 item" do
+    item = Item.create(name: "goat", description: "its a goat", image_url: "www.yahoo.com/goat")
+    Item.create(name: "cow", description: "its a cow", image_url: "www.yahoo.com/cow")
+
+    get "/api/v1/items/#{item.id}"
+
+    expect(response).to be_success
+
+    items = JSON(response.body)
+    expect(items["name"]).to eq("goat")
+    expect(items["description"]).to eq("its a goat")
+    expect(items["image_url"]).to eq("www.yahoo.com/goat")
+    expect(items["name"]).to_not eq("cow")
+  end
 end
