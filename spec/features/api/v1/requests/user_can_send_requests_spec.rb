c1b3b3a2c99c558f17961e7b1cbddf72eae84163
Spencer Carter <scmountain@gmail.com>
2017-04-13 09:15:41 -0600

    setting up controller

---
+++ spec/features/api/v1/requests/user_can_send_requests_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'as a user I can send' do
+  it "can send request, see all items" do
+    item = Item.create(name: "goat", description: "its a goat", image_url: "www.yahoo.com/goat")
+    Item.create(name: "cow", description: "its a cow", image_url: "www.yahoo.com/cow")
+    Item.create(name: "pig", description: "its a pig", image_url: "www.yahoo.com/pig")
+
+    get "/api/v1/items"
+
+    expect(response).to be_success
+
+    items = JSON(response.body)
+    expect(items.count).to eq(3)
+    expect(items.first.name).to eq("goat")
+    expect(items.last.name).to eq("cow")
+    expect(items.last.name).to_not eq("chicken")
+  end
+end
